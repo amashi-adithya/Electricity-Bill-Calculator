ae0cd2db4e58d473b3e45a03acf45e031149b4c7
Amashi <amashigg03@gmail.com>
2026-03-05 21:39:26 +0530

    Added the domestic consumer bill amount calculation

--- calculator.py
+++ calculator.py
@@ -2,3 +2,13 @@ print("Electricity Bill Calculator")
 
 consumer_type = input("Enter consumer type (domestic/corporate/religious): ")
 units = float(input("Enter electricity usage in kWh: "))
+
+bill = 0
+
+if consumer_type == "domestic":
+    if units <= 100:
+        bill = units * 10
+    elif units <= 200:
+        bill = (100 * 10) + (units - 100) * 15
+    else:
+        bill = (100 * 10) + (100 * 15) + (units - 200) * 20

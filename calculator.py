730347de2e10fa2ee005843d4388d4893536be5b
Amashi <amashigg03@gmail.com>
2026-03-05 21:41:38 +0530

    Added the corporate client bill amount calculation

--- calculator.py
+++ calculator.py
@@ -2,3 +2,6 @@ print("Electricity Bill Calculator")
 
 consumer_type = input("Enter consumer type (domestic/corporate/religious): ")
 units = float(input("Enter electricity usage in kWh: "))
+
+elif consumer_type == "corporate":
+    bill = units * 25
